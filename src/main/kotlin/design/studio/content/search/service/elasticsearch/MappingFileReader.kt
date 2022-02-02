f2717671546770ac0f2108d03cf93f12d67a53f1
Yasuyuki Takeo <yasuyuki.takeo@liferay.com>
2022-02-03 07:02:34 +0900

    Add validation test for type mapping structure

--- src/main/kotlin/design/studio/content/search/service/elasticsearch/MappingFileReader.kt
+++ src/main/kotlin/design/studio/content/search/service/elasticsearch/MappingFileReader.kt
@@ -5,23 +5,17 @@ import co.elastic.clients.elasticsearch.indices.GetMappingResponse
 import co.elastic.clients.json.JsonpDeserializer
 import co.elastic.clients.json.JsonpMapper
 import co.elastic.clients.json.jackson.JacksonJsonpMapper
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import java.io.StringReader
 
 /**
  * @author Yasuyuki Takeo
  */
 class MappingFileReader() {
-    companion object {
-        val log: Logger = LoggerFactory.getLogger(MappingFileReader::class.java)
-    }
-
     var mapper: JsonpMapper = JacksonJsonpMapper()
 
     // TODO : Real mapping format is found here:
     // https://www.elastic.co/guide/en/elasticsearch/reference/current/dynamic-templates.html#dynamic-mapping-runtime-fields
-    final val MAPPING_FILE_PATH = "/mappings/liferay-type-mappings.json"
+    final val MAPPING_FILE_PATH = "/mappings/studio-type-mappings.json"
 
     fun getResource(fileName: String) =
         this::class.java.getResource(fileName).readText(Charsets.UTF_8)
--- src/test/kotlin/design/studio/content/search/service/elasticsearch/MappingFileReaderTest.kt
+++ src/test/kotlin/design/studio/content/search/service/elasticsearch/MappingFileReaderTest.kt
@@ -17,10 +17,47 @@ class MappingFileReaderTest : FunSpec() {
             json shouldContain "template_long_sortable"
         }
 
+        test("studio-type-mappings.json Test") {
+            var reader: MappingFileReader = MappingFileReader()
+            var json: String = reader.getResource(reader.MAPPING_FILE_PATH)
+            val mappings = reader.getTypeMappings("studio-index", json)
+            mappings shouldNotBe null
+            if (mappings != null) {
+                mappings.dynamicTemplates()[0].get("template_ja")?.mapping()?.text()?.analyzer() shouldBe "kuromoji"
+                mappings.dynamicTemplates()[0].get("template_ja")?.mapping()?.text()?.store() shouldBe true
+                (mappings.dynamicTemplates()[0].get("template_ja")?.mapping()?.text()?.termVector()?.jsonValue()
+                    ?: "") shouldBe "with_positions_offsets"
+            }
+        }
+
         test("getTypeMappings Test") {
             val json = """{
   "testindex" : {
     "mappings" : {
+        "dynamic_templates": [
+          {
+            "integers": {
+              "match_mapping_type": "long",
+              "mapping": {
+                "type": "integer"
+              }
+            }
+          },
+          {
+            "strings": {
+              "match_mapping_type": "string",
+              "mapping": {
+                "type": "text",
+                "fields": {
+                  "raw": {
+                    "type":  "keyword",
+                    "ignore_above": 256
+                  }
+                }
+              }
+            }
+          }
+        ],    
       "properties" : {
         "id" : {
           "type" : "text",
@@ -61,8 +98,11 @@ class MappingFileReaderTest : FunSpec() {
             val mappings = reader.getTypeMappings("testindex", json)
             mappings shouldNotBe null
             if (mappings != null) {
-                (mappings.properties().get("name")?.isObject() ?: false) shouldBe true
-                (mappings.properties().get("id")?.isText() ?: false) shouldBe true
+                mappings.properties().get("name")?.isObject() shouldBe true
+                mappings.properties().get("id")?.isText() shouldBe true
+                mappings.dynamicTemplates()[0].get("integers")?.matchMappingType() shouldBe "long"
+                mappings.dynamicTemplates()[1].get("strings")?.mapping()?.text()?.fields()?.get("raw")?.keyword()
+                    ?.ignoreAbove() shouldBe 256
             }
         }
     }
